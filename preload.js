7a8e53dc5feb201751617c89891254f189e34d0f
flaribbit <1149761294@qq.com>
2021-08-05 16:54:14 +0800

    electron, version and drag demo

---
+++ preload.js
@@ -0,0 +1,20 @@
+const { contextBridge, ipcRenderer } = require('electron')
+const path = require('path')
+
+contextBridge.exposeInMainWorld('electron', {
+  startDrag: (fileName) => {
+    ipcRenderer.send('ondragstart', fileName)
+  }
+})
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,34 @@
   <h1>{{ msg }}</h1>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Documentation </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button type="button" @click="state.count++">
-    count is: {{ state.count }}
-  </button>
+  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+  <p>
+    We are using Node.js <span id="node-version"></span>, Chromium <span id="chrome-version"></span>, and Electron
+    <span id="electron-version"></span>.
+  </p>
+  <p>Drag the boxes below to somewhere in your OS (Finder/Explorer, Desktop, etc.) to copy this html file.</p>
+  <div draggable="true" style="color: red" @dragstart="dragme">index.html</div>
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
-
+import { reactive } from "vue";
 defineProps({
-  msg: String
-})
-
-const state = reactive({ count: 0 })
+  msg: String,
+});
+const state = reactive({ count: 0 });
+function dragme(event) {
+  event.preventDefault();
+  electron.startDrag("index.html");
+}
 </script>
 
 <style scoped>
