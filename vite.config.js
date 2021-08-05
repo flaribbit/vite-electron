0152ff8b78d5805b3dd81b2481672fa35a707267
flaribbit <1149761294@qq.com>
2021-08-06 00:35:55 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import child_process from 'child_process'
 
-if (process.env.NODE_ENV) {
-  var basepath = '';
-} else {
-  var basepath = './';
+if (!process.env.NODE_ENV) {
   process.env.VITE_DEV = "1";
-  console.log(process.env);
   child_process.exec("electron .");
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: basepath,
+  base: "",
   plugins: [vue()]
 })
